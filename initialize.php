33335b4e564c9d7387b21343f8a8df81124160ca
sql8732324 <kimi20255@hotmail.com>
2024-09-25 22:44:17 +0300

    Update initialize.php

--- initialize.php
+++ initialize.php
@@ -42,7 +42,7 @@ if(!defined('base_url')) define('base_url','https://sql8732324.github.io/dms/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 //if(!defined('dev_data')) define('dev_data',$dev_data);
 if(!defined('DB_SERVER')) define('DB_SERVER',"sql8.freemysqlhosting.net");
-if(!defined('DB_USERNAME')) define('DB_USERNAME',"sql8732324");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
 if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"SSV3BDX3dz");
 if(!defined('DB_NAME')) define('DB_NAME',"sql8732324");
 
