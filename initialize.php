3e2146c6fe2da4833c0b3e99cbd9a0710156a85e
sql8732324 <kimi20255@hotmail.com>
2024-09-26 00:28:56 +0300

    Update initialize.php

--- initialize.php
+++ initialize.php
@@ -38,7 +38,7 @@ if(!defined('DB_NAME')) define('DB_NAME',"sql5731936");
 
 
 $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
-if(!defined('base_url')) define('base_url','http://localhost/dms/');
+if(!defined('base_url')) define('base_url','https://sql8732324.github.io/dms/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 //if(!defined('dev_data')) define('dev_data',$dev_data);
 if(!defined('DB_SERVER')) define('DB_SERVER',"sql8.freemysqlhosting.net");
