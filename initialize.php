7debfedebe2ecffce0ea1a3f1531bf802cbfcf31
sql8732324 <kimi20255@hotmail.com>
2024-09-26 00:25:29 +0300

    Update initialize.php

--- initialize.php
+++ initialize.php
@@ -36,7 +36,7 @@ if(!defined('DB_NAME')) define('DB_NAME',"sql5731936");
 
 <?php 
 
-/*
+
 $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
 if(!defined('base_url')) define('base_url','http://localhost/dms/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
@@ -45,10 +45,10 @@ if(!defined('DB_SERVER')) define('DB_SERVER',"sql8.freemysqlhosting.net");
 if(!defined('DB_USERNAME')) define('DB_USERNAME',"sql8732324");
 if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"SSV3BDX3dz");
 if(!defined('DB_NAME')) define('DB_NAME',"sql8732324");
-*/
+
 ?>
 <?php
-
+/*
 $dev_data = array('id'=>'','firstname'=>'','lastname'=>'','username'=>'dev_oretnom','password'=>'','last_login'=>'','date_updated'=>'','date_added'=>'');
 if(!defined('base_url')) define('base_url','http://localhost/dms/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
@@ -57,5 +57,5 @@ if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
 if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
 if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
 if(!defined('DB_NAME')) define('DB_NAME',"sql5731936");
-
+*/
 ?>
